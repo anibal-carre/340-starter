eb55c06808c9268fe01912021f2d2450a958bedc
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2025-09-06 19:11:12 -0300

    complete home page

--- server.js
+++ server.js
@@ -11,25 +11,24 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
-/* ***********************
- * Routes
- *************************/
-
-//Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
-
-app.use(static)
 
 /* ***********************
  * View Engine and Templates
  *************************/
 app.set("view engine", "ejs")
+app.set("views", __dirname + "/views")
+app.set("layout", "layouts/layout")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
 
+/* ***********************
+ * Routes
+ *************************/
+app.use(static)
 
+//Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
