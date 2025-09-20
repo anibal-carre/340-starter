1f36753531c875da23279c387231b2628b72c6a4
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2025-09-19 22:40:03 -0300

    controllers, models and some modifications

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 
 /* ***********************
@@ -26,9 +27,7 @@ app.use(expressLayouts)
 app.use(static)
 
 //Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
