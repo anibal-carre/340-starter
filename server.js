931e0bc3d2d4ca1f02c0edff0830b0717e91250f
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2025-09-20 17:52:04 -0300

    Details view

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
 const baseController = require("./controllers/baseController")
 
 
@@ -28,6 +29,8 @@ app.use(static)
 
 //Index route
 app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
